b75cf8012d5dc72da2fb4253f08ab1e464b54609
rameshkumarnayak226 <rameshkumarnayak226@gmail.com>
2026-08-24 19:06:34 +0530

    second commit

---
+++ src/demo/CallMyName.java
@@ -0,0 +1,13 @@
+package demo;
+
+import java.util.Scanner;
+
+public class CallMyName {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name=sc.next();
+		
+		System.out.println("hii Mr "+name+" i am your developer Mr Rk");
+	}
+}
